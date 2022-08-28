dcb26f0554f6e98c5f487a7a96e30e8858b0c2ff
Shubham Sharma <sh1nine96@gmail.com>
2022-08-28 07:22:18 +0530

    Top Projects

---
+++ Digital-Clock/script.js
@@ -0,0 +1,31 @@
+const hour = document.getElementById(`hours`);
+const minute = document.getElementById(`minutes`);
+const second = document.getElementById(`seconds`);
+const amPM = document.getElementById(`ampm`);
+
+function updateClock() {
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ap = `AM`
+
+    if (h > 12) {
+        h = h - 12;
+        amPM = `PM`
+    }
+
+    h = h <10? "0" + h: h;
+    m = m <10? "0" + m: m;
+    s = s <10? "0" + s: s;
+
+    hour.innerText = h;
+    minute.innerText = m;
+    second.innerText = s;
+    amPM.innerText = ap;
+
+    setTimeout(() => {
+        updateClock();
+    }, 1000);
+}
+updateClock();
+
