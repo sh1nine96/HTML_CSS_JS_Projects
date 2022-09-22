222cb9b28cc818213b926471724fee55ef4126cb
Shubham Sharma <sh1nine96@gmail.com>
2022-09-22 07:26:30 +0530

    id param added in food-item file

--- Food_Ordering_App_Advance_JS/Food_Item.js
+++ Food_Ordering_App_Advance_JS/Food_Item.js
@@ -3,20 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.FoodItem = void 0;
 const Food_Category_1 = require("./Food_Category");
 class FoodItem {
-    // [x: string]: string;
-    constructor(id, name, category, userRating, price, imagepath) {
-        this.id = id;
-        this.name = name;
-        this.category = category;
-        this.userRating = userRating;
-        this.price = price;
-        this.imagepath = imagepath;
-    }
-    display() {
-        console.log(`name of the food is: ${this.name}`);
-    }
+  // [x: string]: string;
+  constructor(name, category, userRating, price, imagepath) {
+    this.name = name;
+    this.category = category;
+    this.userRating = userRating;
+    this.price = price;
+    this.imagepath = imagepath;
+  }
+  display() {
+    console.log(`name of the food is: ${this.name}`);
+  }
 }
 exports.FoodItem = FoodItem;
-let fc1 = new Food_Category_1.FoodCategory(Food_Category_1.FoodCategory.FOOD_CATEGORY_BIRYANI);
-let foodItem1 = new FoodItem(1, "Ambur Biryani", fc1, 4.5, 10, "");
+let fc1 = new Food_Category_1.FoodCategory(
+  Food_Category_1.FoodCategory.FOOD_CATEGORY_BIRYANI
+);
+let foodItem1 = new FoodItem("Ambur Biryani", fc1, 4.5, 10, "");
 foodItem1.display();
