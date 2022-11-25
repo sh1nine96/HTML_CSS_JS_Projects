d54e817a034d485cd1a074b3cb896d1a23adb5d4
Shubham Sharma <sh1nine96@gmail.com>
2022-11-25 20:45:32 +0530

    html-css-done

---
+++ Basic_Calculator/script.js
@@ -0,0 +1,11 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
